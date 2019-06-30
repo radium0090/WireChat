eb06338df58e1892a859f9f0f37a5e9ef8ff2f12
皮皮 <pp@pipideMacBook-Pro.local>
2019-06-30 15:39:11 +0900

    Initial Commit

---
+++ WireChat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WireChat
+//
+//  Created by 皮皮 on 2019/06/30.
+//  Copyright © 2019 LS-Community. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
